a9a43d67ef8307aa04733fc42df858ff57a02ae1
Jim Lake <jim@blueskylabs.com>
2013-10-22 22:35:13 -0700

    correct paging

--- static/js/reader.js
+++ static/js/reader.js
@@ -32,6 +32,9 @@ var g_story_node = {};
 var g_ejs_story = false;
 var g_curr_page = 0;
 var g_page_count = 0;
+var g_page_list = [];
+var g_reader_width = 0;
+var g_reader_height = 0;
 
 function readerReady()
 {
@@ -50,7 +53,7 @@ function readerReady()
     $(window).resize(function()
     {
         window.scrollTo(0,0);
-        renderStory();
+        maybeRenderStory();
     });
 }
 $(document).ready(readerReady);
@@ -98,53 +101,6 @@ function loadReaderSettings()
     }
 }
 
-function readerSeekToWord(word_num)
-{
-    var new_top = 0;
-    var body_height = parseInt( $('#reader #story_body').css('height') );
-    var scroll_height = $('#reader #story_body').get(0).scrollHeight;
-
-    if( word_num > 0 )
-    {
-        var y_pos = 0;
-        var scroll_top = $('#story_body').scrollTop();
-        $('#reader #story_body .story_text').children().each(function(index,element)
-        {
-            var start_word = $(this).data('start_word');
-            if( start_word <= word_num )
-            {
-                y_pos = scroll_top + $(this).position().top;
-            }
-            else
-            {
-                return false;
-            }
-        });
-        var page = Math.round(y_pos / body_height);
-        
-        new_top = page * body_height;
-    }
-    else
-    {
-        new_top = 0;
-    }
-    
-    if( new_top + body_height < scroll_height )
-    {
-        $('#reader #story_body').scrollTop(new_top);
-    }
-}
-function readerSaveProgress()
-{
-    var in_view = $('#reader #story_body .story_text').children(':in-viewport');
-    
-    var current_word = in_view.first().data('start_word');
-    var last = in_view.last();
-    var max_word = last.data('start_word') + last.data('word_count');
-    
-    streamSaveProgress(current_word,max_word);
-}
-
 function readerValidSwipe(is_left)
 {
     if( is_left )
@@ -181,10 +137,7 @@ function readerClick(x,y)
     }
     else
     {
-        var body_width = $('#reader #story_body').width();
-        var new_left = g_curr_page * body_width;
-        $('#reader #story_body').scrollLeft(new_left);
-
+        scrollToPage(g_curr_page,true);
         clickReaderCenter();
     }
 }
@@ -227,40 +180,63 @@ function isLastStoryPage()
 
 function readerPageRight()
 {
-    var body_width = $('#reader #story_body').width();
-    var scroll_width = $('#reader #story_body').get(0).scrollWidth;
-
     var new_page = g_curr_page + 1;
     if( new_page >= g_page_count )
     {
-        readerNext();
+        if( hasNextStory() )
+        {
+            readerNext();
+        }
+        else
+        {
+            scrollToPage(g_curr_page);
+        }
     }
     else
     {
-        var new_left = new_page * body_width;
-        $('#reader #story_body').animate({ scrollLeft: new_left },PAGE_DELAY);
-        g_curr_page = new_page;
+        scrollToPage(new_page);
     }
     readerHideOverlays(true);
 }
 function readerPageLeft()
 {
-    var scroll_left = $('#reader #story_body').scrollLeft();
-    var body_width = $('#reader #story_body').width();
-    var new_left = scroll_left - body_width;
-    
-    var new_page = g_curr_page - 1;
     if( g_curr_page > 0 )
     {
-        var new_left = body_width * new_page;
-        $('#reader #story_body').animate({ scrollLeft: new_left },PAGE_DELAY);
-        g_curr_page = new_page;
+        var new_page = g_curr_page - 1;
+        scrollToPage(new_page);
     }
     else
     {
-        readerPrev();
+        if( hasPreviousStory() )
+        {
+            readerPrev();
+        }
+        else
+        {
+            scrollToPage(g_curr_page);
+        }
     }
     readerHideOverlays(true);
+}
+function scrollToPage(new_page,instant)
+{
+    var scroll_left = $('#reader #story_body').scrollLeft();
+    var body_width = $('#reader #story_body').width();
+    
+    var new_left = body_width * (new_page + 1);
+    if( instant )
+    {
+        $('#reader #story_body').scrollLeft(new_left);
+    }
+    else
+    {
+        var delay = Math.abs(new_left - scroll_left) / body_width * PAGE_DELAY;
+        delay = Math.min(delay,PAGE_DELAY);
+        $('#reader #story_body').animate({ scrollLeft: new_left },delay);
+    }
+    g_curr_page = new_page;
+
+    readerSaveProgress();
 }
 
 function readerHideOverlays(animate)
@@ -335,8 +311,17 @@ function clickFontLarger()
     }
 }
 
-function renderStory(story,node)
+function maybeRenderStory()
 {
+    if( $(window).width() != g_reader_width
+        || $(window).height() != g_reader_height )
+    {
+        renderStory();
+    }
+}
+function renderStory(story,node,load_end_page)
+{
+    console.log("renderStory");
     if( story )
     {
         g_story_data = story;
@@ -345,21 +330,29 @@ function renderStory(story,node)
     {
         g_story_node = node;
     }
-    g_curr_page = 0;
     var next_story = {
         title: 'Next Story',
         subtitle: '',
         author_list: [ 'Next Author' ]
     };
-    var args = { story: g_story_data, next_story: next_story };
-    g_ejs_story.update('paged_story',args);
 
     readerFixMetrics();
-    var end_of_story = g_ejs_end_of_story.render(args);
-    $('#reader #story_body #paged_story .pages').append(end_of_story);
-    g_page_count++;
-    
+
+    var args = { story: g_story_data, next_story: next_story };
+    makePages(args);
+
     setPageSizes();
+    countPageWords();
+    
+    if( load_end_page )
+    {
+        scrollToPage(g_page_count - 1,true);
+    }
+    else
+    {
+        readerSeekToWord(g_story_node.current_word);
+    }
+    readerSaveProgress();
 
     $('#reader #top_bar .current_title').html(g_story_data.title);
     $('#reader #top_bar .nav_bar div').removeClass('active');
@@ -375,30 +368,92 @@ function renderStory(story,node)
     {
         $('#reader #top_bar .nav_bar .fast_forward').addClass('active');
     }
+    g_reader_width = $(window).width();
+    g_reader_height = $(window).height();
 }
-function ignoreMe()
+function countPageWords()
 {
+    g_page_list = [];
     var word_count = 0;
-    $('#reader #story_body .story_text').children().each(function(index,element)
+    $('#reader #story_body .page').each(function(index,element)
     {
+        var page_col = $(this);
         var words = 0;
-        var text = $(this).text();
-        var m = text.match(/\w[^\s]*/g);
-        if( m )
+        $(this).each(function(index2,element2)
         {
-            words = m.length;
-        }
-        
-        $(this).data('start_word',word_count);
-        $(this).data('word_count',words);
-        
+            if( !$(this).hasClass('ignore_words') )
+            {
+                var text = $(this).text();
+                var m = text.match(/\w[^\s]*/g);
+                if( m )
+                {
+                    words += m.length;
+                }
+            }
+        });
+        var page = {
+            start_word: word_count,
+            word_count: words
+        };
+        g_page_list.push(page);
+
         word_count += words;
     });
-    
-    readerSeekToWord(node.current_word);
-    
-    readerSaveProgress();
+    g_page_list.splice(0,1);
+    g_page_list.splice(-1,1);
 }
+function readerSeekToWord(word_num)
+{
+    var new_page = 0;
+
+    if( word_num > 0 )
+    {
+        for( var i = 0 ; i < g_page_list.length ; ++i )
+        {
+            var page = g_page_list[i];
+            if( word_num == page.start_word )
+            {
+                break;
+            }
+            else if( word_num < page.start_word )
+            {
+                --new_page;
+                break;
+            }
+            else
+            {
+                ++new_page;
+            }
+        }
+    }
+    else
+    {
+        new_page = 0;
+    }
+    
+    new_page = Math.max(0,new_page);
+    new_page = Math.min(new_page,g_page_list.length - 1);
+    
+    scrollToPage(new_page,true);
+}
+function readerSaveProgress()
+{
+    var page = {};
+    if( g_curr_page >= g_page_list.length )
+    {
+        page = g_page_list[g_page_list.length - 1];
+    }
+    else
+    {
+        page = g_page_list[g_curr_page];
+    }
+
+    var current_word = page.start_word;
+    var max_word = page.start_word + page.word_count;
+    
+    streamSaveProgress(current_word,max_word);
+}
+
 function readerFixMetrics()
 {
     var metrics = THEME_METRICS[g_current_theme];
@@ -406,26 +461,20 @@ function readerFixMetrics()
     var font_size = parseInt( $('#story_body').css('font-size') );
     var line_height = Math.ceil( font_size * metrics.line_height_ratio );
     $('#story_body').css('line-height',line_height + 'px');
-    
-    makePages();
 }
-function setPageSizes()
+function makePages(args)
 {
-    var container_width = $('#reader #story_body').width();
-    var padding_width = parseInt( $('#reader #story_body .page').css('padding-left') );
-    var page_width = container_width - 2*padding_width;
-    $('#reader #story_body .page').css('width',page_width + 'px');
-}
+    g_ejs_story.update('paged_story',args);
 
-function makePages()
-{
+    var pages_col = $('#paged_story .pages');
+    var full_story_col = $('#paged_story .pages .full_story');
+    var html = "<div class='page first pad'></div>";
+    pages_col.append(html);
+
     var page_height = $('#story_body_container').height();
     var container_width = $('#reader #story_body').width();
     var padding_width = parseInt( $('#reader #story_body .page').css('padding-left') );
     var page_width = container_width - 2*padding_width;
-
-    var pages_col = $('#paged_story .pages');
-    var full_story_col = $('#paged_story .pages .full_story');
     
     if( !full_story_col || full_story_col.length == 0 )
     {
@@ -514,7 +563,21 @@ function makePages()
             }
         }
     }
+    var end_of_story = g_ejs_end_of_story.render(args);
+    pages_col.append(end_of_story);
+    g_page_count++;
+
+    var html = "<div class='page last pad'></div>";
+    pages_col.append(html);
 }
+function setPageSizes()
+{
+    var container_width = $('#reader #story_body').width();
+    var padding_width = parseInt( $('#reader #story_body .page').css('padding-left') );
+    var page_width = container_width - 2*padding_width;
+    $('#reader #story_body .page').css('width',page_width + 'px');
+}
+
 
 function clickTheme(new_theme)
 {
@@ -541,7 +604,7 @@ function readerPrev()
         streamPrevious();
         getCurrentStory(function(err,story,node)
         {
-            renderStory(story,node);
+            renderStory(story,node,true);
         });
     }
 }
